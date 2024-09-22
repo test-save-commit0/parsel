164c04b529b12d1ddc3374f073b116a27069d306
Wenting Zhao <wentingzhao@outlook.com>
2024-09-22 02:38:55 +0000

    Commit 0

--- parsel/utils.py
+++ parsel/utils.py
@@ -1,10 +1,9 @@
 import re
 from typing import Any, Iterable, Iterator, List, Match, Pattern, Union, cast
-
 from w3lib.html import replace_entities as w3lib_replace_entities
 
 
-def flatten(x: Iterable[Any]) -> List[Any]:
+def flatten(x: Iterable[Any]) ->List[Any]:
     """flatten(sequence) -> list
     Returns a single, flat list which contains all elements retrieved
     from the sequence and all recursively contained sub-sequences
@@ -19,20 +18,16 @@ def flatten(x: Iterable[Any]) -> List[Any]:
     >>> flatten(["foo", ["baz", 42], "bar"])
     ['foo', 'baz', 42, 'bar']
     """
-    return list(iflatten(x))
+    pass
 
 
-def iflatten(x: Iterable[Any]) -> Iterator[Any]:
+def iflatten(x: Iterable[Any]) ->Iterator[Any]:
     """iflatten(sequence) -> Iterator
     Similar to ``.flatten()``, but returns iterator instead"""
-    for el in x:
-        if _is_listlike(el):
-            yield from flatten(el)
-        else:
-            yield el
+    pass
 
 
-def _is_listlike(x: Any) -> bool:
+def _is_listlike(x: Any) ->bool:
     """
     >>> _is_listlike("foo")
     False
@@ -53,44 +48,19 @@ def _is_listlike(x: Any) -> bool:
     >>> _is_listlike(range(5))
     True
     """
-    return hasattr(x, "__iter__") and not isinstance(x, (str, bytes))
+    pass
 
 
-def extract_regex(
-    regex: Union[str, Pattern[str]], text: str, replace_entities: bool = True
-) -> List[str]:
+def extract_regex(regex: Union[str, Pattern[str]], text: str,
+    replace_entities: bool=True) ->List[str]:
     """Extract a list of strings from the given text/encoding using the following policies:
     * if the regex contains a named group called "extract" that will be returned
     * if the regex contains multiple numbered groups, all those will be returned (flattened)
     * if the regex doesn't contain any group the entire regex matching is returned
     """
-    if isinstance(regex, str):
-        regex = re.compile(regex, re.UNICODE)
-
-    if "extract" in regex.groupindex:
-        # named group
-        try:
-            extracted = cast(Match[str], regex.search(text)).group("extract")
-        except AttributeError:
-            strings = []
-        else:
-            strings = [extracted] if extracted is not None else []
-    else:
-        # full regex or numbered groups
-        strings = regex.findall(text)
-
-    strings = flatten(strings)
-    if not replace_entities:
-        return strings
-    return [w3lib_replace_entities(s, keep=["lt", "amp"]) for s in strings]
+    pass
 
 
-def shorten(text: str, width: int, suffix: str = "...") -> str:
+def shorten(text: str, width: int, suffix: str='...') ->str:
     """Truncate the given text to fit in the given width."""
-    if len(text) <= width:
-        return text
-    if width > len(suffix):
-        return text[: width - len(suffix)] + suffix
-    if width >= 0:
-        return suffix[len(suffix) - width :]
-    raise ValueError("width must be equal or greater than 0")
+    pass
--- parsel/xpathfuncs.py
+++ parsel/xpathfuncs.py
@@ -1,14 +1,12 @@
 import re
 from typing import Any, Callable, Optional
-
 from lxml import etree
 from w3lib.html import HTML5_WHITESPACE
-
-regex = f"[{HTML5_WHITESPACE}]+"
+regex = f'[{HTML5_WHITESPACE}]+'
 replace_html5_whitespaces = re.compile(regex).sub
 
 
-def set_xpathfunc(fname: str, func: Optional[Callable]) -> None:  # type: ignore[type-arg]
+def set_xpathfunc(fname: str, func: Optional[Callable]) ->None:
     """Register a custom extension function to use in XPath expressions.
 
     The function ``func`` registered under ``fname`` identifier will be called
@@ -22,37 +20,13 @@ def set_xpathfunc(fname: str, func: Optional[Callable]) -> None:  # type: ignore
     .. _`in lxml documentation`: https://lxml.de/extensions.html#xpath-extension-functions
 
     """
-    ns_fns = etree.FunctionNamespace(None)
-    if func is not None:
-        ns_fns[fname] = func
-    else:
-        del ns_fns[fname]
+    pass
 
 
-def setup() -> None:
-    set_xpathfunc("has-class", has_class)
-
-
-def has_class(context: Any, *classes: str) -> bool:
+def has_class(context: Any, *classes: str) ->bool:
     """has-class function.
 
     Return True if all ``classes`` are present in element's class attr.
 
     """
-    if not context.eval_context.get("args_checked"):
-        if not classes:
-            raise ValueError("XPath error: has-class must have at least 1 argument")
-        for c in classes:
-            if not isinstance(c, str):
-                raise ValueError("XPath error: has-class arguments must be strings")
-        context.eval_context["args_checked"] = True
-
-    node_cls = context.context_node.get("class")
-    if node_cls is None:
-        return False
-    node_cls = " " + node_cls + " "
-    node_cls = replace_html5_whitespaces(" ", node_cls)
-    for cls in classes:
-        if " " + cls + " " not in node_cls:
-            return False
-    return True
+    pass
